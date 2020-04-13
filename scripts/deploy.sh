de54e8fdd273e62488a07c55800a2362fc6fd9f0
trrtly <328602875@qq.com>
2020-04-13 23:31:48 +0800

    chore: add tt

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,8 @@ if [[ $TRAVIS_BRANCH == "master" ]];then
   TARGET_DIR=/data/webroot/test/coupons-front
 fi
 
+if [[ $TRAVIS_BRANCH == "tt" ]];then
+  TARGET_DIR=/data/webroot/product/wmzs
+fi
+
 rsync -r --delete-after --quiet -e "ssh -i ./deploy_key -p 22" ./dist/ root@$SSH_HOST:$TARGET_DIR
